a44f5aba1d559545a39605a57cb1e9ecf39ff19d
cavassani <lucas.cavassani.q@gmail.com>
2025-03-27 12:02:10 -0300

    implementação inicial do servidor + nodes do comfy

---
+++ comfy/nodes.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+import torch
+
+import os
+import sys
+import json
+import hashlib
+import traceback
+import math
+import time
+import random
+import logging
+
+from PIL import Image, ImageOps, ImageSequence
+from PIL.PngImagePlugin import PngInfo
+
+import numpy as np
+import safetensors.torch
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy"))
